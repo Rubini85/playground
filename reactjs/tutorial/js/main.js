55513bae086d7f37a33bbd104e3eec7c338debb5
Alessio Rapini <alessio.rapini@yahoo.it>
2016-03-10 23:35:37 +0100

    split data model from view; two react components

--- reactjs/tutorial/js/main.js
+++ reactjs/tutorial/js/main.js
@@ -1,18 +1,27 @@
+// helpers
+function toReact(contact) {
+    return React.createElement('li', {},
+        React.createElement('h2', {}, contact.name),
+        React.createElement('a', {href: 'mailto: ' + contact.email}, contact.email)
+    );
+}
+
+// main container
 var container = document.getElementById('react-app');
+
+// data model
+var contacts = [
+    {id: 1, name: 'Alessio Rapini', email: '_@rub.me'},
+    {id: 2, name: 'Emanuele Palazzetti', email: '_@palazzem.me'},
+    {id: 3, name: 'Federico Frenguelli', email: '_@frenguelli.me'}
+]
+
+var contactList = contacts.map(toReact);
 
 var rootElement =
     React.createElement('div', {},
         React.createElement('h1', {}, 'Contacts'),
-        React.createElement('ul', {},
-            React.createElement('li', {},
-                React.createElement('h2', {}, 'Alessio Rapini'),
-                React.createElement('a', {href: 'mailto:_@rub.me'}, '_@rub.me')
-            ),
-            React.createElement('li', {},
-                React.createElement('h2', {}, 'Emanuele Palazzetti'),
-                React.createElement('a', {href: 'mailto:_@palazzem.me'}, '_@palazzem.me')
-            )
-        )
+        React.createElement('ul', {}, contactList)
     );
 
 ReactDOM.render(rootElement, container);
