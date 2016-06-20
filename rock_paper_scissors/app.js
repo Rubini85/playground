c406e183100ab0710a043647742e4c8c3305e2b9
Alessio Rapini <alessio.rapini@yahoo.it>
2016-06-21 00:05:12 +0200

    develop the game

--- rock_paper_scissors/app.js
+++ rock_paper_scissors/app.js
@@ -15,6 +15,41 @@ else {
 }
 
 // Render the game on the page
-document.write("user: ", userChoice);
+document.write("User: ", userChoice);
 document.write("<br>");
-document.write("computer: ", computerChoice);
+document.write("Computer: ", computerChoice);
+document.write("<br><br>");
+
+// Call the game function
+game(userChoice, computerChoice);
+
+// Develop the game
+function game(choice1, choice2) {
+    if (choice1 === choice2) {
+        document.write("The result is a draw!");
+    }
+    else if (choice1 === "rock") {
+        if (choice2 === "scissors") {
+            document.write("Rock wins!");
+        }
+        else {
+            document.write("Paper wins!");
+        }
+    }
+    else if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            document.write("Paper wins!");
+        }
+        else {
+            document.write("Scissors wins!");
+        }
+    }
+    else if (choice1 === "scissors") {
+        if (choice2 === "paper") {
+            document.write("Scissors wins!");
+        }
+        else {
+            document.write("Rock wins!");
+        }
+    }
+}
