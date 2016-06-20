b90fbad3275c1650cfb39aa91ee4d5a4e648d72e
Alessio Rapini <alessio.rapini@yahoo.it>
2016-06-20 22:47:34 +0200

    add the computer choice

--- rock_paper_scissors/app.js
+++ rock_paper_scissors/app.js
@@ -1,3 +1,20 @@
 // User makes a choice
 var userChoice = prompt("Do you choose rock, paper or scissors?");
+
+// Computer makes a random choice
+var computerChoice = Math.random();
+
+if (computerChoice < 0.34) {
+    computerChoice = "rock";
+}
+else if (computerChoice < 0.67) {
+    computerChoice = "paper";
+}
+else {
+    computerChoice = "scissors";
+}
+
+// Render the game on the page
 document.write("user: ", userChoice);
+document.write("<br>");
+document.write("computer: ", computerChoice);
